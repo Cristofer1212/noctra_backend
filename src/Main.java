6fcbe6692bb85a9579fd46a737e0f9afe4a2cb82
carlos <carlosvallejoslescano@gmail.com>
2026-06-24 01:51:59 -0500

    first commit by carlos

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // --- PRUEBA QUICK SORT ---
-        int[] arreglo = {55, 18, 72, 6, 40, 29, 88, 13};
-        System.out.println("--- Inicio prueba QuickSort ---");
-        System.out.println("Array original: " + java.util.Arrays.toString(arreglo));
-        // llamar al método
-        quickSort(arreglo, 0, arreglo.length - 1);
-        System.out.println("Array ordenado: " + java.util.Arrays.toString(arreglo));
-        System.out.println("--- Fin prueba QuickSort ---\n");
-        // -------------------------
+
 
 
 
@@ -73,41 +65,11 @@ public class Main {
 
     }
 
-    // --- MÉTODOS DEL ALGORITMO (Copiados de tu imagen) ---
-    public static void  quickSort(int[] arreglo, int low, int high) {
-
-        if (low < high) {
-            int pivote = partition(arreglo, low, high);
-            quickSort(arreglo, low, pivote - 1);
-            quickSort(arreglo,  pivote + 1, high);
-        }
-
-    }
 
 
 
-    // función de apoyo del caso recursivo
 
-    public  static int partition(int[] arreglo ,int low ,int high) {
-        // objetivo: menores, mayores | PIVOTE
-        int pivote = arreglo[high];
-        int i = low - 1; // con quien cabimaos de posicion si el valor es menor que el pivote
-        for (int j = low; j < high; j++) {
-            if ( arreglo[j] <= pivote ) {
-                i++;
-                int temporal = arreglo[i];
-                arreglo[i] = arreglo[j];
-                arreglo[j] = temporal;
-            }
-        }
 
-        // partirlo
-        int temporal = arreglo[ i + 1 ];
-        arreglo[i + 1] = pivote;
-        arreglo[high] = temporal;
-
-        return i + 1;
-    }
 
 
 }
